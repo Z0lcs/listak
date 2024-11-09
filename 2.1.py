8eca372d230bbc52427d94db28fea604252d352d
Arany Zoltán <aranyzoli11@gmail.com>
2024-11-09 16:41:26 +0100

    2.1 save

---
+++ 2.1.py
@@ -0,0 +1,14 @@
+a_betus_szavak = []
+a_szo = None
+
+print('Adj meg egy a-val kezdődő szót, ha nem a-val kezdődik a szó nem tárollom el, ha pedig entert nyomsz vége! ')
+
+while a_szo != '':
+    a_szo = input('Adj meg egy szót! ')
+
+    if a_szo == '':
+        break
+    elif (a_szo[0]) == 'a':
+        a_betus_szavak.append(a_szo)
+
+print(a_betus_szavak)
